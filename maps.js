aad5ff7acc596181ae0a024c747fe2bfb74a6dd5
leandrohyun <101629903+leandrohyun@users.noreply.github.com>
2022-05-28 11:24:56 -0300

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,75 @@
+/*
+Maps será usado para identificar edifícios de uma determinada avenida com base em sua ordem, 
+esses edifícios serão numerados de acordo com sua ordem, onde os números serão usados como 
+“key” para acessar suas informações, como nome, quantidade de moradores e outros como “value”.
+*/
+
+import { defaultToString } from '../util';
+import { ValuePair } from './models/value-pair';
+
+export default class Dictionary {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+  set(key, value) {
+    if (key != null && value != null) {
+      const tableKey = this.toStrFn(key);
+      this.table[tableKey] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
+  }
+  get(key) {
+    const valuePair = this.table[this.toStrFn(key)];
+    return valuePair == null ? undefined : valuePair.value;
+  }
+  hasKey(key) {
+    return this.table[this.toStrFn(key)] != null;
+  }
+  remove(key) {
+    if (this.hasKey(key)) {
+      delete this.table[this.toStrFn(key)];
+      return true;
+    }
+    return false;
+  }
+  values() {
+    return this.keyValues().map(valuePair => valuePair.value);
+  }
+  keys() {
+    return this.keyValues().map(valuePair => valuePair.key);
+  }
+  keyValues() {
+    return Object.values(this.table);
+  }
+  forEach(callbackFn) {
+    const valuePairs = this.keyValues();
+    for (let i = 0; i < valuePairs.length; i++) {
+      const result = callbackFn(valuePairs[i].key, valuePairs[i].value);
+      if (result === false) {
+        break;
+      }
+    }
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
+  size() {
+    return Object.keys(this.table).length;
+  }
+  clear() {
+    this.table = {};
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const valuePairs = this.keyValues();
+    let objString = `${valuePairs[0].toString()}`;
+    for (let i = 1; i < valuePairs.length; i++) {
+      objString = `${objString},${valuePairs[i].toString()}`;
+    }
+    return objString;
+  }
+}
